3cef87fde8064bb2468e786d5efacab898b3e7c9
Noah Huppert <contact@noahh.io>
2019-07-17 11:23:58 -0400

    integrated gh webhook handler & commands.Receiver

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"net/http"
 	"context"
+	"net/http"
 	"os"
 	"os/signal"
 	"sync"
 
-	"github.com/kscout/ops-bot/handlers"
+	"github.com/kscout/ops-bot/commands"
 	"github.com/kscout/ops-bot/config"
-	
+	"github.com/kscout/ops-bot/handlers"
+	"github.com/kscout/ops-bot/messages"
+
 	"github.com/Noah-Huppert/golog"
 )
 
@@ -25,6 +27,9 @@ func main() {
 		cancelFn()
 	}()
 
+	// doneGroup is used to wait for all go routines started in main to finish before exiting
+	// .Add should be called before a go routine is called, .Done should be called when the
+	// go routine ends.
 	var doneGroup sync.WaitGroup
 
 	cfg, err := config.NewConfig()
@@ -32,15 +37,40 @@ func main() {
 		logger.Fatalf("failed to load configuration: %s", err.Error())
 	}
 
+	// {{{1 Setup message processing system
+	var messageBus chan messages.Message
+	var commandReqBus chan commands.CommandRequest
+
+	cmdReceiver := commands.Receiver{
+		MessageBus:    messageBus,
+		CommandReqBus: commandReqBus,
+	}
+
+	doneGroup.Add(1)
+	go func() {
+		defer doneGroup.Done()
+
+		logger.Debug("starting command receiver")
+
+		cmdReceiver.Receive(ctx)
+
+		logger.Debug("stopped command receiver")
+	}()
+
 	// {{{1 Setup HTTP server
-	router := handlers.NewRouter()
-	router.Add(handlers.HealthHandler{})
-	
+	httpRouter := handlers.NewRouter()
+	httpRouter.Add(handlers.HealthHandler{})
+	httpRouter.Add(handlers.GHWebhookHandler{
+		GH:              nil,
+		GHWebhookSecret: cfg.GHWebhookSecret,
+		MessageBus:      messageBus,
+	})
+
 	server := http.Server{
 		Addr: cfg.HTTPAddr,
 		Handler: handlers.PanicHandler{
 			Logger: logger.GetChild("panic"),
-			Hander: router,
+			Hander: httpRouter,
 		},
 	}
 
@@ -57,7 +87,7 @@ func main() {
 	doneGroup.Add(1)
 	go func() {
 		defer doneGroup.Done()
-		
+
 		<-context.Done()
 
 		logger.Debug("closing HTTP server")
