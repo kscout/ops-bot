4cc71ed36a81138015d2daec18716beccac214f1
Noah Huppert <contact@noahh.io>
2019-07-17 11:23:09 -0400

    messages pkg docs: made pkg purpose more clear

--- messages/docs.go
+++ messages/docs.go
@@ -1,7 +1,11 @@
 /*
+Message data model.
+
 The ops bot is designed to receive message from multiple different sources.
 
-The messages package offers a common data format which all message sources 
+The messages package offers a common data format which all message sources
 must convert their messages into.
+
+See commands.Receiver for details on the syntax of commands in Messages.
 */
 package messages
