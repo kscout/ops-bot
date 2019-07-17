e19cad7dc2c4ef0ee602fc61a395e28f9e3828d8
Noah Huppert <contact@noahh.io>
2019-07-16 22:30:40 -0400

    tried new handlers docs bullet format

--- handlers/docs.go
+++ handlers/docs.go
@@ -3,12 +3,12 @@ HTTP handlers.
 
 Unless otherwise specified the following is true of all endpoints:
 
-- No authentication required  
+* No authentication required  
 
-- URLs can have embedded parameters (/api/v0/user/<username>) and query parameters  
+* URLs can have embedded parameters (/users/<username>) and query parameters  
 
-- Request bodies must be JSON formatted  
+* Request bodies must be JSON formatted  
 
-- Response bodies will always be JSON  
+* Response bodies will always be JSON  
 */
 package handlers
